535691248ff247a109fb1b4143a72d83c4ebd885
kkIIun <rlwjd4177@naver.com>
2021-06-02 20:14:05 +0900

    hidding my secret_key

--- proj2/src/api.js
+++ proj2/src/api.js
@@ -1,21 +1,22 @@
 import axios from 'axios';
 
 const ID_KEY = 'MPF85hV_M_MgoJFe46Ms';
-const SECRET_KEY = 'uSvgQms2A1';
+const SECRET_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 const api = axios.create({
   baseURL: '/api',
   headers: {
     'X-Naver-Client-Id': ID_KEY,
     'X-Naver-Client-Secret': SECRET_KEY,
-  }
+  },
 });
 
 export const naverMoviesApi = {
-  search: word => api.get('/v1/search/movie.json', {
-    params: {
-      query: word,
-      display: 10
-    }
-  })
+  search: (word) =>
+    api.get('/v1/search/movie.json', {
+      params: {
+        query: word,
+        display: 10,
+      },
+    }),
 };
